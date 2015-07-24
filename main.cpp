a1d90c861a69dcaeb1d0b7f785bcf43dd9723197
theGerk <benji@altmansoftwaredesign.com>
2015-07-24 10:35:08 -0500

    more functions written

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
-#include <iostream>
+-#include <iostream>
 #include <cstring>
 #include <vector>
 #include <fstream>
 
 //defined constants
 #define PROGRAM_NAME "Jacksan Wail"
+
+//global variables
+bool ReadFromCin = true;
 
 //class declarations:
 class tournament;
@@ -14,7 +17,8 @@ class player;
 class tournament {
 	private:
 		std::vector <player> player_list;			//players
-		std::vector <unsigned int> players_out_of_game; 
+		std::vector <unsigned int> players_out_of_game;
+		std::string name;							//tournament name
 		
 		//gives new game(s)
 		//not started
@@ -38,6 +42,14 @@ class tournament {
 		void end_game(unsigned int player);
 		void end_game(std::string player);
 
+		//constructor
+		//not started
+		tournament(std::string tournament_name);
+		tournament(std::string tournament_name, std::vector <player> players);
+		
+		//main function for tournament (menu)
+		//not started
+		void main();
 };
 
 class player {
@@ -51,17 +63,61 @@ class player {
 //function declarations:
 //opens a saved, unfinished tournament
 //not started
-tournament open(std::ifstream savedFile);
-tournament open(std::string fileLocation);
+tournament load_tournament(std::ifstream savedFile);
+tournament load_tournament(std::string fileLocation);
 
+//reads the help for different areas in program
+//not started
+void main_help();
+void name_new_tournament_help();
+
+//creates a new tournament
+//complete
+tournament add_tournament();					//complete
+tournament add_tournament(std::string input);	//complete
 
 //main function:
 int main()
 {
 	//intro to program
-	std::cout << "Welcome to " << PROGRAM_NAME << ".\nPlease enter the code for what you would like to do.\nTyping \"help\" at any time will give you a list of commands.\n";
+	std::cout << "Welcome to " << PROGRAM_NAME << ".\nPlease enter the command for what you would like to do.\nTyping \"help\" at any time will give you a list of commands and instructions.\n";
+	std::string input;
+	std::vector <tournament> tour;
+	std::ifstream in = std::cin;
+	
+	//where the program runs
+	do {
+		in >> input;
+		
+		//commands:
+		/*
+			help - shows list of commands and prompts to give instruction
+			new - opens new tournament
+			exit - ends program
+			load - loads a tournament
+			enter - enters a loaded tournament
+			read (not yet implemented) - reads from file as if user input
+		*/
+		
+		//find what the command is
+		if(input == "help")
+			main_help();
+		else if(input == "new")
+			tour.push_back(add_tournament());
+		else if(input == "exit")
+			break;
+		else if(input == "enter")
+			enter_tournament(&tour);
+		else if(input == "load")
+			tour.push_back(load_tournament());
+		else
+			std::cout << "not a command, please use \"help\" to get list of commands\n";
+	}while(true);
+	
+	//may want to put in something so that
 	
 	
+	//goodbye
 	return 0;
 }
 
@@ -73,3 +129,22 @@ void tournament::add_player(std::string new_player)
 	a.name = new_player;
 	tournament.player_list.push_back(a);
 }
+
+tournament add_tournament()
+{
+	std::string name;
+	getline(name, std::cin);
+	while(name == "help")
+	{
+		name_new_tournament_help();
+		getline(name, std::cin);
+	}
+	return add_tournament(name);
+}
+
+tournament add_tournament(std::string input)
+{
+	tournament output(input);
+	return output;
+}
+
